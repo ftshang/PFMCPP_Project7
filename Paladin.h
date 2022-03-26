0b84b68e177f816e65e0b1f7d2ed0f6347971fbe
ftshang <74879078+ftshang@users.noreply.github.com>
2022-03-26 05:12:19 +0000

    Implemented Paladin class.

--- Paladin.h
+++ Paladin.h
@@ -1,7 +1,21 @@
 #pragma once
+#include "Character.h"
 
-struct Paladin : Character
+struct Paladin : public Character
 {
 private:
     const std::string name;
+
+public:
+    // Constructor
+    Paladin(std::string name_, int hp_, int armor_, int attackDamage_ = 10);
+
+    // Virtual Destructor
+    virtual ~Paladin() override;
+
+    // Overridden pure virtual function getName
+    virtual const std::string& getName() override;
+
+    // Overriden pure virtual function getStats
+    virtual std::string getStats() override;
 };
