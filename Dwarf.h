415ce1b1d665f151791b1ccb7ee0ae6cc5e8758d
ftshang <74879078+ftshang@users.noreply.github.com>
2022-03-26 04:59:13 +0000

    Implemented Dwarf class.

--- Dwarf.h
+++ Dwarf.h
@@ -1,7 +1,21 @@
 #pragma once
+#include "Character.h"
 
-struct Dwarf : Character
+struct Dwarf : public Character
 {
 private:
     const std::string name;
+
+public:
+    // Constructor
+    Dwarf(std::string name_, int hp_, int armor_, int attackDamage_ = 4);
+
+    // Virtual Destructor
+    virtual ~Dwarf() override;
+
+    // Override the pure virtual function named getName
+    virtual const std::string& getName() override;
+
+    // Override the pure virtual function named getStats
+    virtual std::string getStats() override;
 };
